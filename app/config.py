7c4b2b9c7e2880060dae50120fd3c431e39dbe18
Viren Hirpara <62017836+viru185@users.noreply.github.com>
2025-05-22 22:50:53 +0530

    logging functionatily added.

--- app/config.py
+++ app/config.py
@@ -3,6 +3,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# datasets url
 DATABASE_URL = os.getenv(
     "DATABASE_URL", "postgresql://mattblack:icandoit@localhost:5432/eventsdb"
 )
+
+# app constants
+# Constants path used in the project
+
+LOGS_FILE = "app/logs/app.log"
+LOGS_DIR = "app/logs"
+
+# config related to logging
+LOG_LEVEL = "INFO"
+LOG_TO_FILE = True
+LOG_TO_CONSOLE = True
--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from app.utils.logger import logger
 
 from app.api.routes_event import router as event_router
 
+logger.info("Creating flask app")
 app = FastAPI(title='City Event API')
 
+logger.info("Adding routes to app")
 app.include_router(event_router)
